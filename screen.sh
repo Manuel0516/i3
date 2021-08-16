7f623374e6bdf996be7c5be9e1853d430a072cdb
Manuel Lorente Rodríguez <66995720+Manuel0516@users.noreply.github.com>
2021-08-16 20:48:21 +0000

    Update screen.sh

--- screen.sh
+++ screen.sh
@@ -1,1 +1,3 @@
-xrandr --output DP-2 --mode 1920x1080 --output HDMI-0 --mode 1680x1050 --right-of DP-2
+#You can change the resolution here. Example:
+#
+#xrandr --output DP-2 --mode 1920x1080 --output HDMI-0 --mode 1680x1050 --right-of DP-2
